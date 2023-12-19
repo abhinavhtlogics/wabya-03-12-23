49f8f9680e75070db59f0af8a061349efff5e26c
abhinav3256 <abhinavkumar3256@gmail.com>
2023-12-19 17:23:44 +0530

    changes-19-12-23

--- src/@core/layouts/components/shared-components/Header.tsx
+++ src/@core/layouts/components/shared-components/Header.tsx
@@ -307,7 +307,7 @@ const Header = () => {
       <div className="container">
         <nav className="navbar navbar-expand-lg">
           <div className="container-fluid">
-            <a className="navbar-brand" href="index.html">
+            <a className="navbar-brand" href={`${router.basePath}/coach/dashboard`}>
               <img src="../../images/admin.png" alt="" />
             </a>
             {/*button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button*/}
