3ce45df45924015ebfb8f860e5ed8094133816c6
abhinav3256 <abhinavkumar3256@gmail.com>
2023-12-21 12:10:12 +0530

    changes-21-12-23

--- src/pages/client/joinvideo/[id].tsx
+++ src/pages/client/joinvideo/[id].tsx
@@ -134,33 +134,13 @@ const VideoCallPage = () => {
                 width: '100%',
                 height: '100%',
                 border: 'none',
-                backgroundColor: '#d37aa5',
-              },
-              theme: {
-                colors: {
-                  accent: '#d37aa5',
-                },
+                backgroundColor: '#f6f6f6',
               },
             });
 
             setCallObject(callObj);
 
-            callObj.join({ url: dynamicURL, theme: {
-              colors: {
-                accent: '#d37aa5',
-                
-              },
-            }, });
-
-            call.setTheme({
-              light: {
-                colors: {
-                  accent: '#d37aa5',
-                  backgroundColor: '#001122',
-                },
-              }
-            });
-            
+            callObj.join({ url: dynamicURL });
 
             // Set the iframe as loaded
             setIframeLoaded(true);
